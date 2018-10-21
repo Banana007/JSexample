c56eaaab67a6c88670c6217cafd5aa864d143025
Eugene <eugenefromrus@gmail.com>
2018-10-21 20:56:55 +1000

    fourth trick

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,20 @@
-console.time('looper')
-var x = 0;
-while(x < 20000000) {
-	x++
+const turtle = {
+	name: "Bob",
+	age: 156,
+	legs: 4,
+	food: "milk",
+	shell: true
+};
+
+'bad code'
+function feed(animal) {
+	return `Feed ${animal.name}, ${animal.age}, ${animal.food}`;
 }
-console.timeEnd('looper')
+// console.log(feed(turtle));
+
+
+'good code'
+function feed({name, age, food}) {
+	return `Feed ${name} ${age} ${food}`;
+}
+console.log(feed(turtle));
